c4e7cdc05e87b239d268dda877a3eefc5c3d43d5
aysenurcaglar <aysenurcaglar24@gmail.com>
2024-03-21 14:23:48 +0300

    assignment done

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Player {
+    private String name;
+    private int healthPercentage;
+    private Weapon weapon;
+
+    public Player(String name, int healthPercentage, Weapon weapon) {
+        this.name = name;
+        this.weapon = weapon;
+        if (healthPercentage > 100)
+            this.healthPercentage = 100;
+        else this.healthPercentage = Math.max(healthPercentage, 0);
+
+    }
+
+    public int healthRemaining() {
+        return healthPercentage;
+    }
+
+    public void loseHealth(int damage) {
+        healthPercentage -= damage;
+        if (healthPercentage <= 0) {
+            System.out.println(name + " player has been knocked out of game");
+            healthPercentage = 0;
+        }
+    }
+
+    public void restoreHealth(int healthPotion) {
+        healthPercentage += healthPotion;
+        if (healthPercentage > 100) {
+            healthPercentage = 100;
+        }
+    }
+
+    public int getWeaponDamage() {
+        return weapon.getDamage();
+    }
+
+    public double getWeaponAttackSpeed() {
+        return weapon.getAttackSpeed();
+    }
+}
